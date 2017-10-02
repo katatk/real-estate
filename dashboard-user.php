898f43c2c33465d2942da444fb39aebdeed39cb8
Kat <katatk28@gmail.com>
2017-10-02 17:52:23 +1300

    wishlist is working, pulling from the wishlist table and displaying when the user logs in on their account page

--- dashboard-user.php
+++ dashboard-user.php
@@ -25,6 +25,7 @@ if (!$_SESSION['logged_in']) {
             <table class="table table-striped">
                 <thead>
                     <tr>
+                        <th>Photo</th>
                         <th>Title</th>
                         <th>Type</th>
                         <th>City</th>
@@ -39,16 +40,40 @@ if (!$_SESSION['logged_in']) {
 
                     include('config.php');
                     /* get all the rows of the property ID where the user email matches the user that's logged in */
-                    "SELECT * FROM properties INNER JOIN user_wishlist ON user_wishlist.Property_ID WHERE user_wishlist.Email_Address=?";
+                   
+                    $sql = "SELECT properties.Image_URL, properties.Title, properties.Type, properties.City, properties.Price, properties.Address, properties.Description FROM user_wishlist INNER JOIN properties
+                    ON user_wishlist.Property_ID = properties.Property_ID
+                    WHERE user_wishlist.Email_Address=?";
                     
-                    "SELECT user_wishlist.Property_ID
-                    FROM user_wishlist WHERE Email_Address=?
-                    INNER JOIN properties ON user_wishlist.Property_ID=properties.Property_ID";
+                    $stmt = $db->prepare($sql);
                     
-                   /* $stmt = $db->prepare("SELECT Property_ID FROM user_wishlist WHERE Email_Address=?");
-                    $stmt->bind_param('s', $_SESSION['email_address']);    
+                    if ($stmt === false) {
+                    printf("Errormessage: %s\n", $db->error);
+                    die();
+                    }
+                    
+                    $stmt->bind_param('s', $_SESSION['email_address']);  
+                    
+                    $stmt->execute();
+                    
+                    $results = $stmt->get_result();
+                    
+                    if ($results->num_rows > 0) {
+                        // output data of each row
+                        while($row = $results->fetch_assoc()) {
+                            echo "<tr><td>".$row["Image_URL"]."</td><td>".$row["Title"]."</td><td>".$row["Type"]."</td><td>".$row["City"]."</td><td>".$row["Price"]."</td><td>".$row["Address"]."</td><td>".$row["Description"]."</td></tr>";
+                        }
+                    } else {
+                        echo "You have no properties saved to your wishlist";
+                    }
+                    
+                    // close statement
+                    $stmt->close();
 
-                    // running insert statement
+                    // close the connection
+                    $db->close();
+
+                   /* // running insert statement
                     if ($stmt->execute() === TRUE) {
                         echo "Email checked successfully";
                     } else {
@@ -56,16 +81,12 @@ if (!$_SESSION['logged_in']) {
                     }
 
                     // bind result variables
-                    $stmt->bind_result($stored_property_id);
+                    $stmt->bind_result($stored_property_url, $stored_property_title, $stored_property_type, $stored_property_city, $stored_property_price, $stored_property_address);
 
                     // fetch value
                     $stmt->fetch();
 
-                    // close statement
-                    $stmt->close();
-
-                    // close the connection
-                    $db->close();*/
+                    
 
                     $sql = "SELECT * FROM user_wishlist WHERE Email_Address =  ORDER BY Property_ID";
 
@@ -80,7 +101,7 @@ if (!$_SESSION['logged_in']) {
                         echo "0 results";
                     }
 
-                    mysqli_close($db);
+                    mysqli_close($db); */
                     ?>
                 </tbody>
             </table>
