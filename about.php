badde1fe51dad5f2b3d7d51ac4faf3fe510d204b
Kat <katatk@users.noreply.github.com>
2017-10-17 12:02:22 +1300

    restructured images folder

--- about.php
+++ about.php
@@ -18,7 +18,7 @@ include 'header.php';
                 <div class="col-md-6">
                     <div class="agent-padding">
                         <div class="agent-border">
-                            <img class="agent-image" src="images/agent1.jpeg" alt="agent1">
+                            <img class="agent-image" src="images/agents/agent1.jpeg" alt="agent1">
                             <div class="agent-details">
                                 <div class="agent-name">Grant Hugh</div>
                                 General Manager<br> 0217689925
@@ -30,7 +30,7 @@ include 'header.php';
                 <div class="col-md-6">
                     <div class="agent-padding">
                         <div class="agent-border">
-                            <img class="agent-image" src="images/agent2.jpeg" alt="agent2">
+                            <img class="agent-image" src="images/agents/agent2.jpeg" alt="agent2">
                             <div class="agent-details">
                                 <div class="agent-name">Amanda Stone</div>
                                 Office Administrator<br> 0227964882
@@ -48,7 +48,7 @@ include 'header.php';
                 <div class="col-md-6 order-2">
                     <div class="agent-padding">
                         <div class="agent-border">
-                            <img class="agent-image" src="images/agent3.jpeg" alt="agent3">
+                            <img class="agent-image" src="images/agents/agent3.jpeg" alt="agent3">
                             <div class="agent-details">
                                 <div class="agent-name">Brody Smith</div>
                                 Sales Agent<br> 0217836761
@@ -60,7 +60,7 @@ include 'header.php';
                 <div class="col-md-6 order-1">
                     <div class="agent-padding">
                         <div class="agent-border">
-                            <img class="agent-image" src="images/agent4.jpeg" alt="agent4">
+                            <img class="agent-image" src="images/agents/agent4.jpeg" alt="agent4">
                             <div class="agent-details">
                                 <div class="agent-name">Rachel Davies</div>
                                 Sales Agent<br> 0279891167
@@ -79,7 +79,7 @@ include 'header.php';
                 <div class="col-md-6">
                     <div class="agent-padding">
                         <div class="agent-border">
-                            <img class="agent-image" src="images/agent5.jpeg" alt="agent5">
+                            <img class="agent-image" src="images/agents/agent5.jpeg" alt="agent5">
                             <div class="agent-details">
                                 <div class="agent-name">Mark Downs</div>
                                 Sales Agent<br> 0217876154
@@ -91,7 +91,7 @@ include 'header.php';
                 <div class="col-md-6">
                     <div class="agent-padding">
                         <div class="agent-border">
-                            <img class="agent-image" src="images/agent6.jpeg" alt="agent6">
+                            <img class="agent-image" src="images/agents/agent6.jpeg" alt="agent6">
                             <div class="agent-details">
                                 <div class="agent-name">Bob Sullivan</div>
                                 Sales Agent<br> 0221565178
@@ -110,7 +110,7 @@ include 'header.php';
                 <div class="col-md-6 order-2">
                     <div class="agent-padding">
                         <div class="agent-border">
-                            <img class="agent-image" src="images/agent7.jpeg" alt="agent7">
+                            <img class="agent-image" src="images/agents/agent7.jpeg" alt="agent7">
                             <div class="agent-details">
                                 <div class="agent-name">Kristy Jones</div>
                                 Sales Agent<br> 0217894513
@@ -122,7 +122,7 @@ include 'header.php';
                 <div class="col-md-6 order-1">
                     <div class="agent-padding">
                         <div class="agent-border">
-                            <img class="agent-image" src="images/agent8.jpeg" alt="agent8">
+                            <img class="agent-image" src="images/agents/agent8.jpeg" alt="agent8">
                             <div class="agent-details">
                                 <div class="agent-name">Susan Crow</div>
                                 Sales Agent<br> 0270054511
@@ -141,7 +141,7 @@ include 'header.php';
                 <div class="col-md-6">
                     <div class="agent-padding">
                         <div class="agent-border">
-                            <img class="agent-image" src="images/agent9.jpeg" alt="agent9">
+                            <img class="agent-image" src="images/agents/agent9.jpeg" alt="agent9">
                             <div class="agent-details">
                                 <div class="agent-name">Janet Ross</div>
                                 Sales Agent<br> 0221234145
@@ -153,7 +153,7 @@ include 'header.php';
                 <div class="col-md-6">
                     <div class="agent-padding">
                         <div class="agent-border">
-                            <img class="agent-image" src="images/agent10.jpeg" alt="agent10">
+                            <img class="agent-image" src="images/agents/agent10.jpeg" alt="agent10">
                             <div class="agent-details">
                                 <div class="agent-name">Allan Wood</div>
                                 Sales Agent<br> 0271178769
