a20b98b859d4b4693bf7ba9d1dcf8d056c2bb5bd
taylahayward <taylamareehayward@gmail.com>
2017-10-31 09:30:41 +1300

    add to wishlist error fixed

--- add-wishlist.php
+++ add-wishlist.php
@@ -1,22 +1,34 @@
-<?php 
+<?php
 session_start();
-
+ 
+// this file adds a property to the user's wishlist, runs when heart icon is clicked
+ 
+// if there is nothing in the get parameter, redirect back to the wishlist page (if the user tries to visit add-wishlist.php)
+if (!isset($_GET['id'])) {
+  header('Location: wishlist');
+  die();
+}
+ 
+// create database connection
 include_once 'config.php';
-
+ 
+// check that the user is logged in and is a normal user, not an admin or agent
 if (isset($_SESSION['logged_in']) && $_SESSION['role'] == "User") {
-
-    // if id has been set, edit the current listing, else we are adding a new entry
+ 
+    // if the id is set as the get parameter, run the function
     if (isset($_GET['id'])) {
-        
+   
+    // get id set in get parameter and the email address of the logged in user
     $id = $_GET['id'];
     $email =  $_SESSION['email_address'];
-    
-    $sql = "SELECT Property_ID FROM user_wishlist WHERE Email_Address=?";
    
-     // create query to check if listing is already in wishlist
+    // check the database for an id that matches both that email address and id in the wishlist table
+    $sql = "SELECT Property_ID FROM user_wishlist WHERE Email_Address=? AND Property_ID=?";
+   
     $stmt = $db->prepare($sql);
+       
     // bind parameters
-    $stmt->bind_param('s', $email);
+    $stmt->bind_param('si', $email, $id);
  
      // running insert statement
     if ($stmt->execute() === FALSE) {
@@ -32,12 +44,8 @@ if (isset($_SESSION['logged_in']) && $_SESSION['role'] == "User") {
  
     // close statement
     $stmt->close();
-        
-        
-   echo $stored_id . " " . $id;
-    die();
  
-    // check id is unique
+    // check if id matches one that's in the database
     if ($stored_id == $id) {
  
     $_SESSION['alertMessage'] = "This property is already added to your Wishlist";
@@ -45,41 +53,45 @@ if (isset($_SESSION['logged_in']) && $_SESSION['role'] == "User") {
     // go back to the wishlist page
     header("Location: wishlist");
     die();
-        
+   
+    // if the id's do not match, it is not already in the user's wishlist and can be added
     } elseif ($stored_id != $id) {
-
+ 
     $sql = "INSERT INTO user_wishlist SET Property_ID=?, Email_Address=?";
-        
-    // creates the statement, prepare removes SQL syntax to prevent SQL injection attacks eg someone typing 'DROP table' into a field
+       
     $stmt = $db->prepare($sql);
+       
     $stmt->bind_param('is', $id, $email);
-
+ 
     // running insert statement
     if ($stmt->execute() === false) {
        echo "Error: " . $db->error;
         die();
     }
-
+ 
     // close statement
     $stmt->close();
-                      
-    // show a success message
-     $_SESSION['alertMessage'] = "Property added to wishlist";
-        
+                     
+    // takes the user to their wishlist and shows a success message
+    $_SESSION['alertMessage'] = "Property added to wishlist";
     header("Location: wishlist");
     die();
-    
+   
     }
-} 
+}
 } elseif (isset($_SESSION['logged_in']) && $_SESSION['role'] == "Agent") {
-    // show a success message
+    // show an error message
     $_SESSION['alertMessage'] = "You must be logged in as a user to view your wishlist.";
     header("Location: login");
     die();
-    
+   
 } elseif (!isset($_SESSION['logged_in'])) {
-    // show a success message
+    // show an error message
     $_SESSION['alertMessage'] = "Please log in to add this property to your wishlist.";
+    header("Location: login");
+    die();
+   
+} else {
     header("Location: login");
     die();
 }
